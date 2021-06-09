6e9b57439c658e01e3af3fcc2da229c580f91876
Andres Reyes 👋 <ar97yf@gmail.com>
2021-06-08 21:16:52 -0500

    [ADD] TailwindCSS ✔

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,8 @@ module.exports = {
         token: process.env.GRAPHCMS_TOKEN,
       },
     },
+    "gatsby-plugin-postcss",
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
